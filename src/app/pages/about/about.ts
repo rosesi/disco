767e65865a5f8edcbea61c7ae6c8f8fe9264989d
ls <ls@sztyiot.com>
2019-08-20 16:14:23 +0800

    second commit

---
+++ src/app/pages/about/about.ts
@@ -0,0 +1,14 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+import { PopoverController } from '@ionic/angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html',
+  styleUrls: ['./about.scss'],
+})
+export class AboutPage {
+  conferenceDate = '2047-05-17';
+
+  constructor() { }
+}
---
+++ src/app/pages/schedule/schedule.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+import { SchedulePage } from './schedule';
+import { SchedulePageRoutingModule } from './schedule-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    SchedulePageRoutingModule
+  ],
+  declarations: [
+    SchedulePage
+  ],
+  entryComponents: []
+})
+export class ScheduleModule { }
